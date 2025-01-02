09394da69494aac627fb2b848bbdd8f00aeff63a
webdevmasum <masumislam0018@gmail.com>
2025-01-02 18:18:48 +0600

    course insert null

--- app/Http/Controllers/Web/Backend/Course/CourseController.php
+++ app/Http/Controllers/Web/Backend/Course/CourseController.php
@@ -126,6 +126,8 @@ class CourseController extends Controller
         if ($request->has('lessons')) {
             foreach ($request->lessons as $lessonData) {
 
+                $videoPath = null;
+
                 //! Video Upload
                 if (isset($lessonData['video']) && $lessonData['video']) {
                     $videoFile = $lessonData['video'];
