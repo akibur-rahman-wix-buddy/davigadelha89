88b7ada3c17108d56cada841d4da8bb14e312f69
Tanvir-Siddiqui <t.ikbal.siddiquee@gmail.com>
2025-01-06 14:50:07 +0600

    footer text change

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6 p-0 footer-left">
-                <p class="mb-0">Copyright © 2024 Tivo. All rights reserved.</p>
+                <p class="mb-0">Copyright © 2025. All rights reserved.</p>
             </div>
             <div class="col-md-6 p-0 footer-right">
                 <p class="mb-0">Hand-crafted & made with <i class="fa fa-heart font-danger"></i></p>
